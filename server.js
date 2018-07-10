0a61c5931dd1aad34b23e87b53513bf1bfbb1720
Lewis Luther-Braun <lewis.braun@googlemail.com>
2018-07-10 13:06:32 +0100

    added front web page and started writing API endpoints

--- server.js
+++ server.js
@@ -1,16 +1,52 @@
 const express = require('express')
 const app = express()
 
+//Serves frontend webpage
+app.use(express.static("public"))
+
 //Start server
-app.listen(3000, function(){
+app.listen(3000, ()=>{
     console.log("Server is listening on port 3000. Ready to accept request.")
 })
 
-app.get('/', (req,res) =>{
-    console.log('Obtaining request')
-    res.send('Hello you')
-})
+app.get('/list-hotels', (req,res) =>{
+        let dummyList = [{
+            name:"Area One",
+            rating:9,
+            location:"London"
+        },
+        {
+            name:"Area Two",
+            rating:8,
+            location:"San Francisco"
+        }
+        ]
+        res.send(dummyList)
+    })
 
 
+
+
+
+
+
+
+
+
+
+
+// app.get('/', (req,res) =>{
+//     console.log('Obtaining request')
+//     res.send('Hello you')
+// })
+
+// app.get('/hotels', (req,res) =>{
+//     console.log('Obtaining request')
+//     const hotel = {
+//         name: "Ritz",
+//         rating: 5
+//     }
+//     res.send(hotel)
+// })
 
 //app.post
